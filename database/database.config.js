79fbf24f14e011bf1f7982be7c5b902c92d7904f
Tovintsoa <tovintsoa1337@capri-nc.com>
2023-06-20 11:04:38 +0300

    Rectification database config

--- database/database.config.js
+++ database/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/assignemnt',
+    url: 'mongodb+srv://mb:toto@cluster0.5e6cs7n.mongodb.net/assignments?retryWrites=true&w=majority',
     url_prod: '',
 }
